74c04366e25aef4bd1b949101915850f625a701f
LJHolmes <lukashaupt@gmx.net>
2023-11-30 15:27:48 +0100

    Test

---
+++ UebungLight/Assets/Scripts/PointLightEffect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointLightEffect : MonoBehaviour
+{
+    private void Update()
+    {
+        gameObject.GetComponent<>
+    }
+}
